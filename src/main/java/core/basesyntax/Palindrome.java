c27409c06e85a3ab74af9effc4fde6350a237c71
Vitamin-oht <61590116+Vitamin-oht@users.noreply.github.com>
2020-04-01 20:21:13 +0300

    Solution added

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.toLowerCase().replaceAll("\\W", "");
+        String palindromeVersion = new StringBuilder(text).reverse().toString();
+        return text.equals(palindromeVersion);
     }
 }
